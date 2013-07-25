1bf98834b93265dfed068ed04e71863fd48b59ff
denniss17 <dennisschroer@hotmail.com>
2013-07-25 19:22:40 +0200

    Compiled for 1.6.2; Version updated to 1.0.1

--- bukkitRadar/src/denniss17/bukkitRadar/radars/BaseRadar.java
+++ bukkitRadar/src/denniss17/bukkitRadar/radars/BaseRadar.java
@@ -1,8 +1,8 @@
 package denniss17.bukkitRadar.radars;
 
-import net.minecraft.server.v1_5_R2.Packet207SetScoreboardScore;
+import net.minecraft.server.v1_6_R2.Packet207SetScoreboardScore;
 
-import org.bukkit.craftbukkit.v1_5_R2.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_6_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Objective;
 
