8eaf1b171ada9cd45fecf26cd3133d4d2aebe7c5
Storm <stoi2m1@yahoo.com>
2016-03-23 17:07:46 -0700

    verify user has big radius perm
    if user has be radius perm, radius is set to bigradius config

--- bukkitRadar/src/denniss17/bukkitRadar/radars/PlayerRadar.java
+++ bukkitRadar/src/denniss17/bukkitRadar/radars/PlayerRadar.java
@@ -1,5 +1,6 @@
 package denniss17.bukkitRadar.radars;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.DisplaySlot;
 
@@ -9,7 +10,7 @@ import denniss17.bukkitRadar.BukkitRadar.RadarType;
 public class PlayerRadar extends BaseRadar {
 
 
-	public PlayerRadar(BukkitRadar plugin, Player player) {
+	public PlayerRadar( BukkitRadar plugin, Player player ) {
 		super(plugin, player, plugin.getConfig().getInt("playerradar.radius"));
 		this.type = RadarType.PLAYER_RADAR;
 	}
@@ -25,8 +26,19 @@ public class PlayerRadar extends BaseRadar {
 	}
 	
 	@Override
-	public void reload(){
-		this.radius = plugin.getConfig().getInt("playerradar.radius");
+	public void reload() {
+		if (player.hasPermission("bukkitRadar.bigRadius"))
+			this.radius = plugin.getConfig().getInt("playerradar.bigRadius");
+		else
+			this.radius = plugin.getConfig().getInt("playerradar.radius");
+		
+		
+		Bukkit.broadcastMessage("player: "+ player);
+		Bukkit.broadcastMessage("perm: "+ player.hasPermission("bukkitRadar.bigRadius"));
+		Bukkit.broadcastMessage("config radius: "+ plugin.getConfig().getInt("playerradar.radius"));
+		Bukkit.broadcastMessage("config bigRadius: "+ plugin.getConfig().getInt("playerradar.bigRadius"));
+		Bukkit.broadcastMessage("set radius: "+ this.radius);
+		
 		this.objective.setDisplayName(parseObjectiveName(plugin.getConfig().getString("playerradar.name")));
 	}
 
